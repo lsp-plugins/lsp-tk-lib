5d1908229f9bb08e7ba5f21bbbc33391bd9fdeb4
sadko4u <sadko4u@gmail.com>
2020-07-29 22:07:49 +0300

    Updated CollectionListener

--- include/lsp-plug.in/tk/prop/CollectionListener.h
+++ include/lsp-plug.in/tk/prop/CollectionListener.h
@@ -17,6 +17,14 @@ namespace lsp
         namespace prop
         {
             /**
+             * Callback type for collection listener
+             * @param obj object
+             * @param prop property that triggered event
+             * @param w widget that has been added
+             */
+            typedef void (* collection_cb_t)(void *obj, Property *prop, Widget *w);
+
+            /**
              * Collection listener
              */
             class CollectionListener
@@ -24,9 +32,24 @@ namespace lsp
                 private:
                     CollectionListener &operator = (const CollectionListener &);
 
+                protected:
+                    void               *pAddObj;
+                    collection_cb_t     pAddCb;
+                    void               *pRemObj;
+                    collection_cb_t     pRemCb;
+
                 public:
                     explicit CollectionListener();
                     virtual ~CollectionListener();
+
+                public:
+                    void            bind_add(void *obj, collection_cb_t cb);
+                    void            bind_remove(void *obj, collection_cb_t cb);
+                    void            bind_all(void *obj, collection_cb_t add, collection_cb_t rem);
+
+                    void            unbind_add();
+                    void            unbind_remove();
+                    void            unbind_all();
 
                 public:
                     /**
--- src/main/prop/CollectionListener.cpp
+++ src/main/prop/CollectionListener.cpp
@@ -15,18 +15,67 @@ namespace lsp
         {
             CollectionListener::CollectionListener()
             {
+                pAddObj     = NULL;
+                pAddCb      = NULL;
+                pRemObj     = NULL;
+                pRemCb      = NULL;
             }
 
             CollectionListener::~CollectionListener()
             {
+                unbind_all();
+            }
+
+            void CollectionListener::bind_add(void *obj, collection_cb_t cb)
+            {
+                pAddObj     = obj;
+                pAddCb      = cb;
+            }
+
+            void CollectionListener::bind_remove(void *obj, collection_cb_t cb)
+            {
+                pRemObj     = obj;
+                pRemCb      = cb;
+            }
+
+            void CollectionListener::bind_all(void *obj, collection_cb_t add, collection_cb_t rem)
+            {
+                pAddObj     = obj;
+                pAddCb      = add;
+                pRemObj     = obj;
+                pRemCb      = rem;
+            }
+
+            void CollectionListener::unbind_add()
+            {
+                pAddObj     = NULL;
+                pAddCb      = NULL;
+            }
+
+            void CollectionListener::unbind_remove()
+            {
+                pRemObj     = NULL;
+                pRemCb      = NULL;
+            }
+
+            void CollectionListener::unbind_all()
+            {
+                pAddObj     = NULL;
+                pAddCb      = NULL;
+                pRemObj     = NULL;
+                pRemCb      = NULL;
             }
 
             void CollectionListener::add(Property *prop, Widget *w)
             {
+                if (pAddCb != NULL)
+                    pAddCb(pAddObj, prop, w);
             }
 
             void CollectionListener::remove(Property *prop, Widget *w)
             {
+                if (pRemCb != NULL)
+                    pRemCb(pRemObj, prop, w);
             }
         }
     }
