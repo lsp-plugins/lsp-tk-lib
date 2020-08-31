751fb420c715aad3063bac34f7f3316cc0b6f6bb
sadko4u <sadko4u@gmail.com>
2020-08-31 20:56:02 +0300

    Submitted skeleton of the GraphDot widget

--- include/lsp-plug.in/tk/widgets/graph/GraphDot.h
+++ include/lsp-plug.in/tk/widgets/graph/GraphDot.h
@@ -21,8 +21,58 @@ namespace lsp
             public:
                 static const w_class_t    metadata;
 
+            protected:
+                enum flags_t
+                {
+                    F_EDITABLE      = 1 << 0,
+                    F_HIGHLIGHT     = 1 << 1,
+                    F_EDITING       = 1 << 2,
+                    F_FINE_TUNE     = 1 << 3
+                };
+
+                typedef struct param_t
+                {
+                    GraphDot                   *pDot;
+                    prop::Boolean               sEditable;      // Editable flag
+                    prop::RangeFloat            sValue;         // Value
+                    prop::StepFloat             sStep;          // Step
+
+                    explicit                param_t(GraphDot *dot, prop::Listener *lst);
+                    void                    property_changed(Property *prop);
+                    void                    init(const char *prefix);
+                } param_t;
+
             private:
                 GraphDot & operator = (const GraphDot &);
+
+            protected:
+                param_t                     sHValue;            // Horizontal value
+                param_t                     sVValue;            // Vertical value
+                param_t                     sZValue;            // The value associated with scrolling
+
+                prop::Integer               sOrigin;            // Index of origin widget
+                prop::Integer               sHAxis;             // Horizontal axis
+                prop::Integer               sVAxis;             // Vertical axis
+                prop::Integer               sSize;              // Size of the dot
+                prop::Integer               sHoverSize;         // Size of the dot (when hover)
+                prop::Integer               sBorderSize;        // Border size
+                prop::Integer               sHoverBorderSize;   // Border size (when hover)
+                prop::Integer               sGap;               // Gap size
+                prop::Integer               sHoverGap;          // Gap size (when hover)
+                prop::Color                 sColor;             // Color of the dot
+                prop::Color                 sHoverColor;        // Color of the dot (when hover)
+                prop::Color                 sBorderColor;       // Color of the border
+                prop::Color                 sHoverBorderColor;  // Color of the border (when hover)
+
+                size_t                      nXFlags;            // Extra flags
+                size_t                      nMBState;           // Mouse button state
+                ssize_t                     nMouseX;            // Mouse initial X position
+                ssize_t                     nMouseY;            // Mouse initial Y position
+
+            protected:
+                void                        apply_motion(ssize_t x, ssize_t y, size_t flags);
+
+                static status_t             slot_on_change(Widget *sender, void *ptr, void *data);
 
             protected:
                 virtual void                property_changed(Property *prop);
@@ -35,7 +85,44 @@ namespace lsp
                 virtual void                destroy();
 
             public:
+                LSP_TK_PROPERTY(RangeFloat,         hvalue,             &sHValue.sValue)
+                LSP_TK_PROPERTY(Boolean,            heditable,          &sHValue.sEditable)
+                LSP_TK_PROPERTY(StepFloat,          hstep,              &sHValue.sStep)
+                LSP_TK_PROPERTY(RangeFloat,         vvalue,             &sVValue.sValue)
+                LSP_TK_PROPERTY(Boolean,            veditable,          &sVValue.sEditable)
+                LSP_TK_PROPERTY(StepFloat,          vstep,              &sVValue.sStep)
+                LSP_TK_PROPERTY(RangeFloat,         zvalue,             &sZValue.sValue)
+                LSP_TK_PROPERTY(Boolean,            zeditable,          &sZValue.sEditable)
+                LSP_TK_PROPERTY(StepFloat,          zstep,              &sZValue.sStep)
+
+                LSP_TK_PROPERTY(Integer,            origin,             &sOrigin)
+                LSP_TK_PROPERTY(Integer,            haxis,              &sHAxis)
+                LSP_TK_PROPERTY(Integer,            vaxis,              &sVAxis)
+                LSP_TK_PROPERTY(Integer,            size,               &sSize)
+                LSP_TK_PROPERTY(Integer,            hover_size,         &sHoverSize)
+                LSP_TK_PROPERTY(Integer,            border_size,        &sBorderSize)
+                LSP_TK_PROPERTY(Integer,            hover_border_size,  &sHoverBorderSize)
+                LSP_TK_PROPERTY(Color,              color,              &sColor)
+                LSP_TK_PROPERTY(Color,              hover_color,        &sHoverColor)
+                LSP_TK_PROPERTY(Color,              border_color,       &sBorderColor)
+                LSP_TK_PROPERTY(Color,              hover_border_color, &sHoverBorderColor)
+
+            public:
                 virtual void                render(ws::ISurface *s, const ws::rectangle_t *area, bool force);
+
+                virtual bool                inside(ssize_t x, ssize_t y);
+
+                virtual status_t            on_mouse_in(const ws::event_t *e);
+
+                virtual status_t            on_mouse_out(const ws::event_t *e);
+
+                virtual status_t            on_mouse_down(const ws::event_t *e);
+
+                virtual status_t            on_mouse_up(const ws::event_t *e);
+
+                virtual status_t            on_mouse_move(const ws::event_t *e);
+
+                virtual status_t            on_change();
         };
     }
 }
--- src/main/widgets/graph/GraphDot.cpp
+++ src/main/widgets/graph/GraphDot.cpp
@@ -6,6 +6,7 @@
  */
 
 #include <lsp-plug.in/tk/tk.h>
+#include <lsp-plug.in/common/debug.h>
 
 namespace lsp
 {
@@ -13,9 +14,70 @@ namespace lsp
     {
         const w_class_t GraphDot::metadata             = { "GraphDot", &GraphItem::metadata };
 
-        GraphDot::GraphDot(Display *dpy):
-            GraphItem(dpy)
+        GraphDot::param_t::param_t(GraphDot *dot, prop::Listener *lst):
+            sEditable(lst),
+            sValue(lst),
+            sStep(lst)
         {
+            pDot        = dot;
+        }
+
+        void GraphDot::param_t::property_changed(Property *prop)
+        {
+            if (sEditable.is(prop))
+                pDot->query_draw();
+            if (sValue.is(prop))
+                pDot->query_draw();
+            if (sStep.is(prop))
+                { /* Nothing */ }
+        }
+
+        void GraphDot::param_t::init(const char *prefix)
+        {
+            Style *style  = pDot->style();
+            LSPString id;
+            id.fmt_ascii("%s.editable");
+            sEditable.bind(&id, style);
+            id.fmt_ascii("%s.value");
+            sValue.bind(&id, style);
+            id.fmt_ascii("%s.step");
+            sStep.bind(&id, style);
+
+            Style *sclass = pDot->style_class();
+            if (sclass != NULL)
+            {
+                id.fmt_ascii("%s.editable");
+                sEditable.init(sclass, false);
+                id.fmt_ascii("%s.value");
+                sValue.init(sclass, 0.0f, -1.0f, 1.0f);
+                id.fmt_ascii("%s.step");
+                sStep.init(sclass, 1.0f, 10.0f, 0.1f);
+            }
+        }
+
+        GraphDot::GraphDot(Display *dpy):
+            GraphItem(dpy),
+            sHValue(this, &sProperties),
+            sVValue(this, &sProperties),
+            sZValue(this, &sProperties),
+            sOrigin(&sProperties),
+            sHAxis(&sProperties),
+            sVAxis(&sProperties),
+            sSize(&sProperties),
+            sHoverSize(&sProperties),
+            sBorderSize(&sProperties),
+            sHoverBorderSize(&sProperties),
+            sGap(&sProperties),
+            sHoverGap(&sProperties),
+            sColor(&sProperties),
+            sHoverColor(&sProperties),
+            sBorderColor(&sProperties),
+            sHoverBorderColor(&sProperties)
+        {
+            nXFlags             = 0;
+            nMBState            = 0;
+            nMouseX             = 0;
+            nMouseY             = 0;
 
             pClass              = &metadata;
         }
@@ -34,13 +96,42 @@ namespace lsp
             if (res != STATUS_OK)
                 return res;
 
+            // Init values
+            sHValue.init("hvalue");
+            sVValue.init("vvalue");
+            sZValue.init("zvalue");
+
             // Init style
-            // TODO
+            sOrigin.bind("origin", &sStyle);
+            sHAxis.bind("haxis", &sStyle);
+            sVAxis.bind("vaxis", &sStyle);
+            sSize.bind("size", &sStyle);
+            sHoverSize.bind("hover.size", &sStyle);
+            sBorderSize.bind("border.size", &sStyle);
+            sHoverBorderSize.bind("hover.border.size", &sStyle);
+            sGap.bind("gap", &sStyle);
+            sHoverGap.bind("hover.gap", &sStyle);
+            sColor.bind("color", &sStyle);
+            sHoverColor.bind("hover.color", &sStyle);
+            sBorderColor.bind("border.color", &sStyle);
+            sHoverBorderColor.bind("hover.border.color", &sStyle);
 
             Style *sclass = style_class();
             if (sclass != NULL)
             {
-                // TODO
+                sOrigin.init(sclass, 0);
+                sHAxis.init(sclass, 0);
+                sVAxis.init(sclass, 0);
+                sSize.init(sclass, 4);
+                sHoverSize.init(sclass, 4);
+                sBorderSize.init(sclass, 12);
+                sHoverBorderSize.init(sclass, 12);
+                sGap.init(sclass, 1);
+                sHoverGap.init(sclass, 1);
+                sColor.init(sclass, "#cccccc");
+                sHoverColor.init(sclass, "#ffffff");
+                sBorderColor.init(sclass, "#cccccc");
+                sHoverBorderColor.init(sclass, "#ffffff");
             }
 
             return STATUS_OK;
@@ -49,10 +140,96 @@ namespace lsp
         void GraphDot::property_changed(Property *prop)
         {
             GraphItem::property_changed(prop);
+            sHValue.property_changed(prop);
+            sVValue.property_changed(prop);
+            sZValue.property_changed(prop);
+
+            if (sOrigin.is(prop))
+                query_draw();
+            if (sHAxis.is(prop))
+                query_draw();
+            if (sVAxis.is(prop))
+                query_draw();
+            if (sSize.is(prop))
+                query_draw();
+            if (sHoverSize.is(prop))
+                query_draw();
+            if (sBorderSize.is(prop))
+                query_draw();
+            if (sHoverBorderSize.is(prop))
+                query_draw();
+            if (sGap.is(prop))
+                query_draw();
+            if (sHoverGap.is(prop))
+                query_draw();
+            if (sColor.is(prop))
+                query_draw();
+            if (sHoverColor.is(prop))
+                query_draw();
+            if (sBorderColor.is(prop))
+                query_draw();
+            if (sHoverBorderColor.is(prop))
+                query_draw();
         }
 
         void GraphDot::render(ws::ISurface *s, const ws::rectangle_t *area, bool force)
         {
+        }
+
+        status_t GraphDot::slot_on_change(Widget *sender, void *ptr, void *data)
+        {
+            GraphDot *_this = widget_ptrcast<GraphDot>(ptr);
+            return (_this != NULL) ? _this->on_change() : STATUS_BAD_ARGUMENTS;
+        }
+
+        status_t GraphDot::on_change()
+        {
+            lsp_trace("hvalue = %f, vvalue=%f, zvalue=%f",
+                    sHValue.sValue.get(),
+                    sVValue.sValue.get(),
+                    sZValue.sValue.get()
+                );
+            return STATUS_OK;
+        }
+
+        void GraphDot::apply_motion(ssize_t x, ssize_t y, size_t flags)
+        {
+        }
+
+        bool GraphDot::inside(ssize_t x, ssize_t y)
+        {
+            // TODO
+            return false;
+        }
+
+        status_t GraphDot::on_mouse_in(const ws::event_t *e)
+        {
+            // TODO
+            return STATUS_OK;
+        }
+
+        status_t GraphDot::on_mouse_out(const ws::event_t *e)
+        {
+            // TODO
+            return STATUS_OK;
+        }
+
+        status_t GraphDot::on_mouse_down(const ws::event_t *e)
+        {
+            // TODO
+            return STATUS_OK;
+        }
+
+        status_t GraphDot::on_mouse_up(const ws::event_t *e)
+        {
+            // TODO
+            return STATUS_OK;
+        }
+
+        status_t GraphDot::on_mouse_move(const ws::event_t *e)
+        {
+            // TODO
+            return STATUS_OK;
         }
     }
 }
