1d0e6fbfef450c0f1d3ab4f1288e70b72f23fc11
Vladimir Sadovnikov <sadko4u@gmail.com>
2025-02-11 21:42:45 +0300

    Added inactive state and colors for tk::MenuItem

--- src/main/widgets/simple/MenuItem.cpp
+++ src/main/widgets/simple/MenuItem.cpp
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2020 Linux Studio Plugins Project <https://lsp-plug.in/>
- *           (C) 2020 Vladimir Sadovnikov <sadko4u@gmail.com>
+ * Copyright (C) 2025 Linux Studio Plugins Project <https://lsp-plug.in/>
+ *           (C) 2025 Vladimir Sadovnikov <sadko4u@gmail.com>
  *
  * This file is part of lsp-tk-lib
  * Created on: 18 сент. 2017 г.
@@ -31,33 +31,75 @@ namespace lsp
         {
             LSP_TK_STYLE_IMPL_BEGIN(MenuItem, Widget)
                 // Bind
+                style::MenuItemColors *c = &vColors[style::MENUITEM_NORMAL];
+                c->sBgSelectedColor.bind("bg.selected.color", this);
+                c->sTextColor.bind("text.color", this);
+                c->sTextSelectedColor.bind("text.selected.color", this);
+                c->sCheckColor.bind("check.color", this);
+                c->sCheckBgColor.bind("check.bg.color", this);
+                c->sCheckBorderColor.bind("check.border.color", this);
+
+                c = &vColors[style::MENUITEM_INACTIVE];
+                c->sBgSelectedColor.bind("inactive.bg.selected.color", this);
+                c->sTextColor.bind("inactive.text.color", this);
+                c->sTextSelectedColor.bind("inactive.text.selected.color", this);
+                c->sCheckColor.bind("inactive.check.color", this);
+                c->sCheckBgColor.bind("inactive.check.bg.color", this);
+                c->sCheckBorderColor.bind("inactive.check.border.color", this);
+
                 sTextAdjust.bind("text.adjust", this);
                 sType.bind("type", this);
                 sChecked.bind("checked", this);
-                sBgSelectedColor.bind("bg.selected.color", this);
-                sTextColor.bind("text.color", this);
-                sTextSelectedColor.bind("text.selected.color", this);
-                sCheckColor.bind("check.color", this);
-                sCheckBgColor.bind("check.bg.color", this);
-                sCheckBorderColor.bind("check.border.color", this);
+                sActive.bind("active", this);
                 sShortcut.bind("shortcut", this);
+
                 // Configure
+                c = &vColors[style::MENUITEM_NORMAL];
+                c->sBgSelectedColor.set("#000088");
+                c->sTextColor.set("#000000");
+                c->sTextSelectedColor.set("#ffffff");
+                c->sCheckColor.set("#00ccff");
+                c->sCheckBgColor.set("#ffffff");
+                c->sCheckBorderColor.set("#000000");
+
+                c = &vColors[style::MENUITEM_INACTIVE];
+                c->sBgSelectedColor.set("#888888");
+                c->sTextColor.set("#444444");
+                c->sTextSelectedColor.set("#cccccc");
+                c->sCheckColor.set("#888888");
+                c->sCheckBgColor.set("#cccccc");
+                c->sCheckBorderColor.set("#000000");
+
                 sTextAdjust.set(TA_NONE);
                 sType.set(MI_NORMAL);
                 sChecked.set(false);
-                sBgSelectedColor.set("#000088");
-                sTextColor.set("#000000");
-                sTextSelectedColor.set("#ffffff");
-                sCheckColor.set("#00ccff");
-                sCheckBgColor.set("#ffffff");
-                sCheckBorderColor.set("#000000");
+                sActive.set(true);
                 sShortcut.clear();
+
                 // Override
                 sPadding.set(16, 16, 2, 2);
+
                 // Commit
                 sPadding.override();
             LSP_TK_STYLE_IMPL_END
             LSP_TK_BUILTIN_STYLE(MenuItem, "MenuItem", "root");
+
+            void MenuItemColors::listener(tk::prop::Listener *listener)
+            {
+                sBgSelectedColor.listener(listener);
+                sTextColor.listener(listener);
+                sTextSelectedColor.listener(listener);
+                sCheckColor.listener(listener);
+                sCheckBgColor.listener(listener);
+                sCheckBorderColor.listener(listener);
+            }
+
+            bool MenuItemColors::property_changed(Property *prop)
+            {
+                return prop->one_of(
+                    sBgSelectedColor, sTextColor, sTextSelectedColor, sCheckColor,
+                    sCheckBgColor, sCheckBorderColor);
+            }
         }
 
         const w_class_t MenuItem::metadata      = { "MenuItem", &Widget::metadata };
@@ -69,15 +111,13 @@ namespace lsp
             sTextAdjust(&sProperties),
             sType(&sProperties),
             sChecked(&sProperties),
-            sBgSelectedColor(&sProperties),
-            sTextColor(&sProperties),
-            sTextSelectedColor(&sProperties),
-            sCheckColor(&sProperties),
-            sCheckBgColor(&sProperties),
-            sCheckBorderColor(&sProperties),
+            sActive(&sProperties),
             sShortcut(&sProperties)
         {
             pClass      = &metadata;
+
+            for (size_t i=0; i<style::MENUITEM_TOTAL; ++i)
+                vColors[i].listener(&sProperties);
         }
         
         MenuItem::~MenuItem()
@@ -103,16 +143,27 @@ namespace lsp
             if (res != STATUS_OK)
                 return res;
 
+            style::MenuItemColors *c = &vColors[style::MENUITEM_NORMAL];
+            c->sBgSelectedColor.bind("bg.selected.color", &sStyle);
+            c->sTextColor.bind("text.color", &sStyle);
+            c->sTextSelectedColor.bind("text.selected.color", &sStyle);
+            c->sCheckColor.bind("check.color", &sStyle);
+            c->sCheckBgColor.bind("check.bg.color", &sStyle);
+            c->sCheckBorderColor.bind("check.border.color", &sStyle);
+
+            c = &vColors[style::MENUITEM_INACTIVE];
+            c->sBgSelectedColor.bind("inactive.bg.selected.color", &sStyle);
+            c->sTextColor.bind("inactive.text.color", &sStyle);
+            c->sTextSelectedColor.bind("inactive.text.selected.color", &sStyle);
+            c->sCheckColor.bind("inactive.check.color", &sStyle);
+            c->sCheckBgColor.bind("inactive.check.bg.color", &sStyle);
+            c->sCheckBorderColor.bind("inactive.check.border.color", &sStyle);
+
             sTextAdjust.bind("text.adjust", &sStyle);
             sText.bind(&sStyle, pDisplay->dictionary());
             sType.bind("type", &sStyle);
             sChecked.bind("checked", &sStyle);
-            sBgSelectedColor.bind("bg.selected.color", &sStyle);
-            sTextColor.bind("text.color", &sStyle);
-            sTextSelectedColor.bind("text.selected.color", &sStyle);
-            sCheckColor.bind("check.color", &sStyle);
-            sCheckBgColor.bind("check.bg.color", &sStyle);
-            sCheckBorderColor.bind("check.border.color", &sStyle);
+            sActive.bind("active", &sStyle);
             sShortcut.bind("shortcut", &sStyle);
             sMenu.bind(NULL);
 
@@ -121,14 +172,35 @@ namespace lsp
             return (id >= 0) ? STATUS_OK : -id;
         }
 
+        style::MenuItemColors *MenuItem::select_colors()
+        {
+            size_t flags = (sActive.get()) ? style::MENUITEM_NORMAL : style::MENUITEM_INACTIVE;
+            return &vColors[flags];
+        }
+
         void MenuItem::property_changed(Property *prop)
         {
             Widget::property_changed(prop);
 
-            if (prop->one_of(sTextAdjust, sText, sType))
-                query_resize();
+            // Self properties
+            style::MenuItemColors *colors = select_colors();
+            if (colors->property_changed(prop))
+                query_draw();
+
+            if (sActive.is(prop))
+            {
+                query_draw();
+
+                // Call parent menu for redraw
+                tk::Menu *parent = widget_cast<tk::Menu>(this->parent());
+                if (parent != NULL)
+                    parent->query_draw(REDRAW_CHILD | REDRAW_SURFACE);
+            }
+
             if (prop->one_of(sChecked))
                 query_draw();
+            if (prop->one_of(sTextAdjust, sText, sType))
+                query_resize();
         }
 
         status_t MenuItem::on_submit()
