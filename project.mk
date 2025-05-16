9500c446d19a9fc9d3fca15717ece08af77497de
Vladimir Sadovnikov <sadko4u@gmail.com>
2025-05-16 22:09:50 +0300

    Preparing for release

--- project.mk
+++ project.mk
@@ -23,5 +23,5 @@ ARTIFACT_ID                 = LSP_TK_LIB
 ARTIFACT_NAME               = lsp-tk-lib
 ARTIFACT_DESC               = Graphical toolkit library for Linux Studio Plugins Project
 ARTIFACT_HEADERS            = lsp-plug.in
-ARTIFACT_VERSION            = 1.0.28-devel
+ARTIFACT_VERSION            = 1.0.28
 ARTIFACT_EXPORT_ALL         = 1
