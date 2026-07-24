7296083a5a087009f31d79591574e15b674c9e53
Vladimir Sadovnikov <sadko4u@gmail.com>
2026-07-24 20:33:07 +0300

    Added 'bundle' build target

--- project.mk
+++ project.mk
@@ -24,4 +24,4 @@ ARTIFACT_NAME               = lsp-tk-lib
 ARTIFACT_DESC               = Graphical toolkit library for Linux Studio Plugins Project
 ARTIFACT_HEADERS            = lsp-plug.in
 ARTIFACT_VERSION            = 1.0.35-devel
-ARTIFACT_EXPORT_ALL         = 1
+ARTIFACT_EXPORT_SYMBOLS     = 1
